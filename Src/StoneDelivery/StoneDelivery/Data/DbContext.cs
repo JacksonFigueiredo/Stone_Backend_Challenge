679667a98b07a46e5a26066cbc27ad78ff639277
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2021-03-11 18:41:18 -0300

    Adicionando Contexto.

---
+++ Src/StoneDelivery/StoneDelivery/Data/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoneDelivery.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneDelivery.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+
+    }
+}
